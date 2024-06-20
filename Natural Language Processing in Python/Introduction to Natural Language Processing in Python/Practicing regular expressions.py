2b51ec61ce66b27e2484b9ec42e49addc3de0348
Brandi H <61725592+branaphy@users.noreply.github.com>
2024-06-20 16:25:35 +0200

    Update Practicing regular expressions.py

--- Natural Language Processing in Python/Introduction to Natural Language Processing in Python/Practicing regular expressions.py
+++ Natural Language Processing in Python/Introduction to Natural Language Processing in Python/Practicing regular expressions.py
@@ -16,3 +16,18 @@ print(re.split(spaces, my_string))
 digits = r"\d+"
 print(re.findall(digits, my_string))
 
+# Search for the first occurrence of "coconuts" in scene_one: match
+match = re.search("coconuts", scene_one)
+
+# Print the start and end indexes of match
+print(match.start(), match.end())
+
+# Write a regular expression to search for anything in square brackets: pattern1
+pattern1 = r"\[.*\]"
+
+# Use re.search to find the first text in square brackets
+print(re.search(pattern1, scene_one))
+
+# Find the script notation at the beginning of the fourth sentence and print it
+pattern2 = r"[\w+, \s+]+:"
+print(re.match(pattern2, sentences[3]))
