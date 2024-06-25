a68f229d9ae49625826809f241f0529fa183b4e2
Brandi H <61725592+branaphy@users.noreply.github.com>
2024-06-25 13:58:46 +0200

    Update Word cloud of tweets.py

--- Natural Language Processing in Python/Sentiment Analysis in Python/Word cloud of tweets.py
+++ Natural Language Processing in Python/Sentiment Analysis in Python/Word cloud of tweets.py
@@ -1,21 +1,6 @@
 # Import the word cloud function and stop words list
 from wordcloud import WordCloud, STOPWORDS 
 
-# Define and update the list of stopwords
-my_stop_words = ____.____(['airline', 'airplane'])
-
-# Create and generate a word cloud image
-my_cloud = WordCloud(____=my_stop_words).generate(text_tweet)
-
-# Display the generated wordcloud image
-plt.imshow(my_cloud, interpolation='bilinear') 
-plt.axis("off")
-# Don't forget to show the final image
-plt.show()
-
-# Import the word cloud function and stop words list
-from wordcloud import WordCloud, STOPWORDS 
-
 # Define the list of stopwords
 my_stop_words = STOPWORDS.update(['airline', 'airplane'])
 
